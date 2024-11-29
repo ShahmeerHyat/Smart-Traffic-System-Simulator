435e743889febd305163e787deec5f98a83f3c68
Saim <i232065@isb.nu.edu.pk>
2024-11-29 19:29:18 +0500

    Add files via upload
    added graph header file

---
+++ graph.h
@@ -0,0 +1,57 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+template<typename T>
+const int INF = 1000000000;
+typedef unsigned int size_t;
+
+template<typename T>
+class Graph {
+private:
+public:
+    Graph()
+    {
+
+    }
+
+    ~Graph()
+    {
+
+    }
+    void addNode(const T& value)
+    {
+
+    }
+
+    void addEdge(int node1, int node2, bool isDirected = false)
+    {
+
+    }
+    
+    void depthFirstSearch(int startNode)
+    {
+
+    }
+    
+    void breadthFirstSearch(int startNode)
+    {
+
+    }
+    
+    bool hasEdge(int node1, int node2) const
+    {
+
+    }
+
+    size_t nodeCount() const
+    {
+
+    }
+
+    size_t edgeCount() const
+    {
+        
+    }
+};
+
+#endif
